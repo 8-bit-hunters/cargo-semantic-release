f5e0ef22f70d4ba9f63df6145979f6f40fad53f4
Kristof Kovacs <kristof.kovacs1996@gmail.com>
2025-01-07 22:35:04 +0100

    :sparkles: Print out the commits in the directory where the program was called

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
+use git2::{Commit, Repository};
 use std::env;
 
-fn main() -> std::io::Result<()> {
-    let path = env::current_dir()?;
+fn main() {
+    let path = env::current_dir().expect("Failed to get current directory");
     println!("Current directory: {}", path.display());
-    Ok(())
+
+    let git_repo = Repository::open(path).expect("Failed to open git repo");
+    let mut revwalk = git_repo.revwalk().expect("Failed to revwalk");
+    revwalk.push_head().expect("Failed to push head");
+
+    let commits: Vec<Commit> = revwalk
+        .filter_map(|object_id| object_id.ok())
+        .filter_map(|valid_object_id| git_repo.find_commit(valid_object_id).ok())
+        .collect();
+
+    println!("Commits in the directory:");
+    for commit in commits {
+        println!(
+            "\t{} - {}",
+            commit.id(),
+            commit.message().unwrap().trim_end()
+        );
+    }
 }
