319c7e76f1d094b69d79dae9c59b39712cb80707
Kristof Kovacs <kristof.kovacs1996@gmail.com>
2025-02-24 21:57:00 +0100

    :white_check_mark: Add end-to-end integration tests

---
+++ tests/integration.rs
@@ -0,0 +1,79 @@
+use cargo_semantic_release::test_util::repo_init;
+pub use cargo_semantic_release::test_util::RepositoryTestExtensions;
+use cargo_semantic_release::{Changes, SemanticVersionAction};
+
+#[test]
+fn empty_repo_keeps_semantic_version() {
+    // Given
+    let (_temp_dir, repository) = repo_init(None);
+
+    // When
+    let result = Changes::from_repo(&repository).define_action_for_semantic_version();
+
+    // Then
+    assert_eq!(result, SemanticVersionAction::Keep);
+}
+
+#[test]
+fn major_change_increments_major_semantic_version() {
+    // Given
+    let commit_messages = vec![
+        "💥 introduce breaking changes",
+        ":sparkles: introduce new feature",
+        ":green_heart: fix CI build",
+        ":memo: add or update documentation",
+    ];
+    let (_temp_dir, repository) = repo_init(Some(commit_messages));
+
+    // When
+    let result = Changes::from_repo(&repository).define_action_for_semantic_version();
+
+    // Then
+    assert_eq!(result, SemanticVersionAction::IncrementMajor);
+}
+
+#[test]
+fn minor_change_increments_minor_semantic_version() {
+    // Given
+    let commit_messages = vec![
+        ":sparkles: introduce new feature",
+        ":green_heart: fix CI build",
+        ":memo: add or update documentation",
+    ];
+    let (_temp_dir, repository) = repo_init(Some(commit_messages));
+
+    // When
+    let result = Changes::from_repo(&repository).define_action_for_semantic_version();
+
+    // Then
+    assert_eq!(result, SemanticVersionAction::IncrementMinor);
+}
+
+#[test]
+fn patch_change_increments_patch_semantic_version() {
+    // Given
+    let commit_messages = vec![
+        ":green_heart: fix CI build",
+        ":memo: add or update documentation",
+    ];
+    let (_temp_dir, repository) = repo_init(Some(commit_messages));
+
+    // When
+    let result = Changes::from_repo(&repository).define_action_for_semantic_version();
+
+    // Then
+    assert_eq!(result, SemanticVersionAction::IncrementPatch);
+}
+
+#[test]
+fn other_change_keeps_semantic_version() {
+    // Given
+    let commit_messages = vec![":memo: add or update documentation"];
+    let (_temp_dir, repository) = repo_init(Some(commit_messages));
+
+    // When
+    let result = Changes::from_repo(&repository).define_action_for_semantic_version();
+
+    // Then
+    assert_eq!(result, SemanticVersionAction::Keep);
+}
