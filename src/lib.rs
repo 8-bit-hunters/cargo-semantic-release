9dc4244079d39e33d18966f8615cec62bfa388fb
Kristof Kovacs <kristof.kovacs1996@gmail.com>
2025-02-24 21:56:37 +0100

    :wrench: Configure test_util module to be public during testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 mod changes;
 mod commit_fetcher;
 mod conventional_commit;
-mod test_util;
+#[cfg(any(test, feature = "test_util"))]
+pub mod test_util;
 mod version_tag;
 
 pub use crate::changes::Changes;
 pub use crate::changes::SemanticVersionAction;
+pub use crate::commit_fetcher::RepositoryFetchCommitExtension;
