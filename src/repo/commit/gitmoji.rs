74ad4b70265b1596d778e61d7424ef46f33fb1ac
Krizsi96 <kristof.kovacs1996@gmail.com>
2025-03-28 20:46:32 +0000

    :truck: Restructure commit module

--- src/repo/commit/gitmoji.rs
+++ src/repo/commit/gitmoji.rs
@@ -1,90 +1,6 @@
 use git2::Commit;
-use once_cell::sync::Lazy;
 use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
-
-/// A structure to represent a git commit.
-///
-/// Can be created with [`from_git2_commit`] method
-#[derive(Clone, Debug, PartialEq, Hash, Eq)]
-pub struct ConventionalCommit {
-    pub message: String,
-    pub hash: String,
-}
-
-impl ConventionalCommit {
-    /// Create [`Commit`] from [`git2::Commit`] object.
-    ///
-    /// [`Commit`]: ConventionalCommit
-    /// ['git2::Commit`]: git2::Commit
-    pub fn from_git2_commit(commit: git2::Commit) -> Self {
-        Self {
-            message: commit.message().unwrap().to_string(),
-            hash: commit.id().to_string(),
-        }
-    }
-
-    /// Return a reference to the `message` attribute
-    pub fn message(&self) -> &str {
-        &self.message
-    }
-}
-
-impl Display for ConventionalCommit {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let short_hash = self
-            .hash
-            .get(0..7)
-            .unwrap_or("Error: can't show short hash");
-        write!(f, "{} - {}", self.message.trim_end(), short_hash)
-    }
-}
-
-#[cfg(test)]
-mod conventional_commit_tests {
-    use crate::repo::ConventionalCommit;
-    use crate::test_util::{repo_init, RepositoryTestExtensions};
-
-    #[test]
-    fn create_from_git2_commit() {
-        // Given
-        let commit_messages = vec!["initial commit"];
-        let (_temp_dir, repository) = repo_init(Some(commit_messages));
-        let git2_commit = repository.find_commit_by_message("initial commit").unwrap();
-
-        // When
-        let result = ConventionalCommit::from_git2_commit(git2_commit.clone());
-
-        // Then
-        let expected_result = ConventionalCommit {
-            message: git2_commit.message().unwrap().to_string(),
-            hash: git2_commit.id().to_string(),
-        };
-        assert_eq!(result, expected_result)
-    }
-
-    #[test]
-    fn display_formatting() {
-        // Given
-        let commit_messages = vec!["initial commit"];
-        let (_temp_dir, repository) = repo_init(Some(commit_messages));
-        let git2_commit = repository.find_commit_by_message("initial commit").unwrap();
-        let conventional_commit = ConventionalCommit::from_git2_commit(git2_commit.clone());
-
-        // When
-        let print_out = format!("{}", conventional_commit);
-
-        // Then
-        assert_eq!(
-            print_out,
-            format!(
-                "{} - {}",
-                git2_commit.message().unwrap(),
-                git2_commit.id().to_string().get(0..7).unwrap()
-            )
-        )
-    }
-}
 
 #[derive(Debug, Eq, PartialEq, Clone, Hash)]
 struct GitmojiCommit {
@@ -133,7 +49,7 @@ impl Display for GitmojiCommit {
 
 #[cfg(test)]
 mod gitmoji_commit_tests {
-    use crate::repo::conventional_commit::{Gitmoji, GitmojiCommit};
+    use crate::repo::commit::gitmoji::{Gitmoji, GitmojiCommit};
     use crate::test_util::{repo_init, RepositoryTestExtensions};
 
     #[test]
@@ -268,6 +184,7 @@ enum Gitmoji {
 
 impl Gitmoji {
     fn gitmoji_map() -> &'static HashMap<Gitmoji, Emoji> {
+        use once_cell::sync::Lazy;
         use Gitmoji::*;
         static GITMOJIS: Lazy<HashMap<Gitmoji, Emoji>> = Lazy::new(|| {
             HashMap::from([
@@ -420,7 +337,7 @@ impl Emoji {
 
 #[cfg(test)]
 mod test_gitmoji {
-    use crate::repo::conventional_commit::Gitmoji;
+    use crate::repo::commit::gitmoji::Gitmoji;
 
     #[test]
     fn display_formatting() {
---
+++ src/repo/commit/mod.rs
@@ -0,0 +1,86 @@
+use std::fmt::Display;
+
+mod gitmoji;
+
+/// A structure to represent a git commit.
+///
+/// Can be created with [`from_git2_commit`] method
+#[derive(Clone, Debug, PartialEq, Hash, Eq)]
+pub struct ConventionalCommit {
+    pub message: String,
+    pub hash: String,
+}
+
+impl ConventionalCommit {
+    /// Create [`Commit`] from [`git2::Commit`] object.
+    ///
+    /// [`Commit`]: ConventionalCommit
+    /// ['git2::Commit`]: git2::Commit
+    pub fn from_git2_commit(commit: git2::Commit) -> Self {
+        Self {
+            message: commit.message().unwrap().to_string(),
+            hash: commit.id().to_string(),
+        }
+    }
+
+    /// Return a reference to the `message` attribute
+    pub fn message(&self) -> &str {
+        &self.message
+    }
+}
+
+impl Display for ConventionalCommit {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let short_hash = self
+            .hash
+            .get(0..7)
+            .unwrap_or("Error: can't show short hash");
+        write!(f, "{} - {}", self.message.trim_end(), short_hash)
+    }
+}
+
+#[cfg(test)]
+mod conventional_commit_tests {
+    use crate::repo::ConventionalCommit;
+    use crate::test_util::{repo_init, RepositoryTestExtensions};
+
+    #[test]
+    fn create_from_git2_commit() {
+        // Given
+        let commit_messages = vec!["initial commit"];
+        let (_temp_dir, repository) = repo_init(Some(commit_messages));
+        let git2_commit = repository.find_commit_by_message("initial commit").unwrap();
+
+        // When
+        let result = ConventionalCommit::from_git2_commit(git2_commit.clone());
+
+        // Then
+        let expected_result = ConventionalCommit {
+            message: git2_commit.message().unwrap().to_string(),
+            hash: git2_commit.id().to_string(),
+        };
+        assert_eq!(result, expected_result)
+    }
+
+    #[test]
+    fn display_formatting() {
+        // Given
+        let commit_messages = vec!["initial commit"];
+        let (_temp_dir, repository) = repo_init(Some(commit_messages));
+        let git2_commit = repository.find_commit_by_message("initial commit").unwrap();
+        let conventional_commit = ConventionalCommit::from_git2_commit(git2_commit.clone());
+
+        // When
+        let print_out = format!("{}", conventional_commit);
+
+        // Then
+        assert_eq!(
+            print_out,
+            format!(
+                "{} - {}",
+                git2_commit.message().unwrap(),
+                git2_commit.id().to_string().get(0..7).unwrap()
+            )
+        )
+    }
+}
