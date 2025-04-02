40eb36e0da3cfd8f866ca8f38c651d23b53752ee
Krizsi96 <kristof.kovacs1996@gmail.com>
2025-04-02 20:33:33 +0000

    :boom: Use new commit types in Changes structure

--- src/repo/mod.rs
+++ src/repo/mod.rs
@@ -2,29 +2,35 @@ mod commit;
 mod commit_fetcher;
 mod version_tag;
 
+use crate::repo::commit::{Commit};
 use crate::repo::commit_fetcher::{fetch_all_commits, fetch_commits_until};
+use crate::repo::prelude::RepositoryExtension;
 use crate::repo::version_tag::get_latest_version_tag;
-pub use commit::ConventionalCommit;
 use git2::{Oid, Repository};
 use std::error::Error;
-pub use version_tag::VersionTag;
+use version_tag::VersionTag;
 
-pub trait RepositoryExtension {
-    fn fetch_commits_until(&self, stop_oid: Oid)
-        -> Result<Vec<ConventionalCommit>, Box<dyn Error>>;
-    fn fetch_all_commits(&self) -> Result<Vec<ConventionalCommit>, Box<dyn Error>>;
-    fn get_latest_version_tag(&self) -> Result<Option<VersionTag>, Box<dyn Error>>;
+pub mod prelude {
+    pub use crate::repo::commit::Commit;
+    pub use crate::repo::commit::CommitInterface;
+    pub use crate::repo::commit::{GitmojiCommit, Gitmoji};
+    pub use crate::repo::version_tag::VersionTag;
+    use git2::Oid;
+    use std::error::Error;
+
+    pub trait RepositoryExtension {
+        fn fetch_commits_until(&self, stop_oid: Oid) -> Result<Vec<Commit>, Box<dyn Error>>;
+        fn fetch_all_commits(&self) -> Result<Vec<Commit>, Box<dyn Error>>;
+        fn get_latest_version_tag(&self) -> Result<Option<VersionTag>, Box<dyn Error>>;
+    }
 }
 
 impl RepositoryExtension for Repository {
-    fn fetch_commits_until(
-        &self,
-        stop_oid: Oid,
-    ) -> Result<Vec<ConventionalCommit>, Box<dyn Error>> {
+    fn fetch_commits_until(&self, stop_oid: Oid) -> Result<Vec<Commit>, Box<dyn Error>> {
         fetch_commits_until(self, stop_oid)
     }
 
-    fn fetch_all_commits(&self) -> Result<Vec<ConventionalCommit>, Box<dyn Error>> {
+    fn fetch_all_commits(&self) -> Result<Vec<Commit>, Box<dyn Error>> {
         fetch_all_commits(self)
     }
 
